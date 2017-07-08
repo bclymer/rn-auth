9233ccafbff9f6a38e2eb2b74b3a7f33e53aeddd
Brian Clymer <crdnilfan@gmail.com>
2017-07-08 14:09:40 -0500

    Finished the app I guess

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
 
     componentWillMount() {
         firebase.initializeApp({
@@ -15,16 +16,55 @@ class App extends Component {
             storageBucket: "rn-auth-11b46.appspot.com",
             messagingSenderId: "446251372834"
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    logout() {
+        firebase.auth().signOut()
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Button onPress={this.logout}>
+                        Log Out
+                    </Button>
+                )
+            case false:
+                return <LoginForm />
+            default:
+                return (
+                    <View style={styles.spinnerContainerStyle}>
+                        <Spinner size="large" />
+                    </View>
+                )
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
 }
+
+const styles = {
+    spinnerContainerStyle: {
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+};
 
 export default App;
